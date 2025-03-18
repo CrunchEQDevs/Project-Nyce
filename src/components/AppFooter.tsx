a0e65bdb344542abf0cf3514e74135803e7a53cf
MatheusCrunch <m.masago@crunchequation.com>
2025-03-18 16:50:56 +0000

    Header & Footer start

---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react";
+
+export default function AppFooter() {
+  return (
+    <footer className="bg-black text-white py-4 fixed bottom-0 w-full z-10">
+      <div className="container mx-auto px-4">
+        <h1>Teste</h1>
+      </div>
+    </footer>
+  );
+}
